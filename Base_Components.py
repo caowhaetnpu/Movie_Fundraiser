540f8bdd4b3f27ede12d8f07ddfd8afc8ff642a2
caowhaetnpu <little.cohen@awatapu.school.nz>
2021-07-07 12:48:11 +1200

    Base Structure

---
+++ Base_Components.py
@@ -0,0 +1,9 @@
+# Ask the user if they are by there self's or if they are with other people and how many
+# Ask for the users name and also if there are more people with them ask for there names too
+# Get the user's age and if there are people with the user ask for the age's too so that the ticket cost can be calculated
+# Ensure that the maximum # of 150 tickets is not exceeded
+# Ask the user what snacks they would like (e.g the type of snack and how many portions)
+# Work out how many of each snack has been ordered
+# Calculate the cost of tickets and snacks
+# Ask for the payment method
+# Work out the total profit
