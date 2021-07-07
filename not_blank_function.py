11a2a0e0dfe36be8bad63ed7bbdd56a53a8929c7
caowhaetnpu <little.cohen@awatapu.school.nz>
2021-07-07 13:23:51 +1200

    Base Structure

---
+++ not_blank_function.py
@@ -0,0 +1,16 @@
+# Functions go there
+
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response!= "":
+            return response
+        else:
+            print("Sorry - this can't be blank")
+
+
+name = not_blank("Name: ")
+
