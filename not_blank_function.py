99e927c0cadc772ef34eb8b3c5b64eda8e3fa742
caowhaetnpu <little.cohen@awatapu.school.nz>
2021-07-07 13:26:13 +1200

    not_blank_function v1

--- not_blank_function.py
+++ not_blank_function.py
@@ -9,7 +9,7 @@ def not_blank(question):
         if response!= "":
             return response
         else:
-            print("Sorry - this can't be blank")
+            print("Sorry, this can't be blank")
 
 
 name = not_blank("Name: ")
