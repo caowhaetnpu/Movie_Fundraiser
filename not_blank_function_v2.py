0275d33de991274becd53a2cab6d56ea15d226f9
caowhaetnpu <little.cohen@awatapu.school.nz>
2021-07-26 14:56:12 +1200

    not_blank_function_v2

---
+++ not_blank_function_v2.py
@@ -0,0 +1,18 @@
+# Functions go there
+
+def not_blank(question, error_message):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response!= "":
+            return response
+        else:
+            print(error_message)
+
+
+name = not_blank("Name: ",
+                 "Sorry - this cant be blank,"
+                 "Please enter your name?")
+
