f1366defd6a54529e53d9a57013c5ff29b327616
caowhaetnpu <little.cohen@awatapu.school.nz>
2021-07-26 14:44:47 +1200

    get_input v1

---
+++ get_input.py
@@ -0,0 +1,12 @@
+# Start of loop
+
+# initialise loop so that it runs at least once
+name = ""
+count = 0
+max_tickets = 5
+
+while name!= "xxx" and count <= max_tickets:
+
+# get details
+    name = input("Name:")
+    count += 1
