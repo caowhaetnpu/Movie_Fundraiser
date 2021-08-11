99afb34f244912c50b648918bf1e67c5f62030c2
caowhaetnpu <little.cohen@awatapu.school.nz>
2021-08-12 10:06:25 +1200

    Base_Components V2
    Ticket price integration

--- Base_Components.py
+++ Base_Components.py
@@ -1,9 +1,81 @@
-# Ask the user if they are by there self's or if they are with other people and how many
-# Ask for the users name and also if there are more people with them ask for there names too
-# Get the user's age and if there are people with the user ask for the age's too so that the ticket cost can be calculated
-# Ensure that the maximum # of 150 tickets is not exceeded
-# Ask the user what snacks they would like (e.g the type of snack and how many portions)
-# Work out how many of each snack has been ordered
-# Calculate the cost of tickets and snacks
-# Ask for the payment method
-# Work out the total profit
+# functions
+
+# checks the ticket is not blank
+def not_blank(question, error_message):
+    valid = False
+
+    while not valid:
+        response = input(question)
+# if name is not blank , program continues
+        if response != "":
+            return response
+# if ticket is blank, shows error ( and repeats the loop)
+        else:
+            print(error_message)
+
+def int_check(question):
+
+    error = "Please enter a whole number between 12 and 130"
+    valid = False
+    while not valid:
+
+#ask user for number and check it is valid
+        try:
+            response = int(input(question))
+
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+
+# if an integer is not entered, display an error
+        except ValueError:
+            print(error)
+
+
+# loop to get ticket details
+max_tickets = 5
+name = ""
+ticket_count = 0
+ticket_sales = 0
+
+
+while name != "quit" and ticket_count < max_tickets:
+
+    name = not_blank("Name: ",
+                     "Sorry - this cant be blank,"
+                     "Please enter your name?")
+    if name == "quit":
+        break
+
+# get age between 12 - 130
+    age = int_check("Age:")
+# check that age is valid
+    if age < 12:
+        print("Sorry your are to young to see this movie")
+        continue
+    elif age > 130:
+        print("That is very old - looks like that was a mistake")
+        continue
+
+    if age < 16:
+        ticket_price = 7.5
+    elif age < 65:
+        ticket_price = 10.5
+    else:
+        ticket_price = 6.5
+
+    ticket_count += 1
+    ticket_sales += ticket_price
+# calculate ticket profit
+ticket_profit = ticket_sales - (5 * ticket_count)
+print("Ticket profit: ${:.2f}".format(ticket_profit))
+
+if ticket_count == max_tickets:
+    print("You have reached max tickets")
+else:
+    print("You have sold {} tickets. \n"
+          "There are {} places still available"
+          .format(ticket_count, max_tickets - ticket_count))
