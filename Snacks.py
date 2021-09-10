f8253138c25632f53a413842af4300f10695c187
caowhaetnpu <little.cohen@awatapu.school.nz>
2021-09-10 12:08:03 +1200

    Lockdown

---
+++ Snacks.py
@@ -0,0 +1,9 @@
+names = ['Rangi', 'Manaia', 'Talia', 'Arihi', 'Fetu']
+
+popcorn = []
+mms = []
+pita_chips = []
+water = []
+orange_juice = []
+
+snacks_lists = [popcorn, mms, pita_chips, water, orange_juice]
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hi, Github")
+# function goes here
+
+def int_check(question, low_num, high_num):
+
+    error = "Please enter a whole number between {} " \
+            "and {}".format(low_num, high_num)
+    valid = False
+    while not valid:
+
+#ask user for number and check it is valid
+        try:
+            response = int(input(question))
+
+
+            if response < 0:
+                return response
+            else:
+                print (error)
+
+
+# if an integer is not entered, display an error
+        except ValueError:
+            print(error)
+
+
+# main routine goes here
+age = int_check("Age:", 12, 130
